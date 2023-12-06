edffab68dc41f2526675a5fa07d5587fbeb502f9
Tameem Bin Haider <haidertameem@gmail.com>
2023-12-06 09:57:26 +0600

    day 2 part 2

--- day-2/solution.hs
+++ day-2/solution.hs
@@ -1,21 +1,23 @@
-parseCube::String -> (Int, Int, Int)
+import Data.Maybe (fromJust)
+parseCube::String -> Maybe (Int, Int, Int)
 parseCube cubeInput = case break (== ' ') cubeInput of
   (count, _:color) -> case color of 
-    "red" -> (read count, 0, 0)
-    "green" -> (0, read count, 0)
-    "blue" -> (0, 0, read count)
-    _ -> (0, 0, 0)
-  _ -> (0, 0, 0)
+    "red" -> Just (read count, 0, 0)
+    "green" -> Just (0, read count, 0)
+    "blue" -> Just (0, 0, read count)
+    _ -> Nothing
+  _ -> Nothing
 
-max'::(Int, Int, Int) -> (Int, Int, Int) -> (Int, Int, Int)
-max' (a, b, c) (d, e, f) = (max a d, max b e, max c f)
+max'::Maybe (Int, Int, Int) -> Maybe (Int, Int, Int) -> Maybe (Int, Int, Int)
+max' (Just (a, b, c)) (Just (d, e, f)) = Just (max a d, max b e, max c f)
+max' _ _ = Nothing
 
-parseSet::String -> (Int, Int, Int)
+parseSet::String -> Maybe (Int, Int, Int)
 parseSet setInput = case break (== ',') setInput of
   (cubeOne, _:_:restOfCubes) -> parseCube cubeOne `max'` parseSet restOfCubes
   (cube, []) -> parseCube cube
 
-parseGame::String -> (Int, Int, Int)
+parseGame::String -> Maybe (Int, Int, Int)
 parseGame gameInput = case break (== ';') gameInput of
   (setOne, _:_:restOfSets) -> parseSet setOne `max'` parseGame restOfSets
   (set, []) -> parseSet set
@@ -27,9 +29,10 @@ parseId gameId = case break (== ' ') gameId of
 
 parseGameWithId::String -> (Int, (Int, Int, Int))
 parseGameWithId gameWithId = case break (== ':') gameWithId of
-  (gameId, _:_:game) -> (parseId gameId, parseGame game)
+  (gameId, _:_:game) -> (parseId gameId, fromJust $ parseGame game)
   _ -> (0, (0, 0, 0))
 
 main = do
   input <- readFile "day-2/input.txt"
   print $ sum $ map fst $ filter (\(id, (r, g, b)) -> r <= 12 && g <= 13 && b <= 14) $ map parseGameWithId $ lines input
+  print $ sum $ map ((\(r, g, b) -> r * g * b) . snd . parseGameWithId) $ lines input
